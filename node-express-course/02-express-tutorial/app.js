a7df0e159dd9d8b00639a23a9d9577a63bdf4b81
VINAY CHAURASIYA <92705023+VinayChaurasiyaA@users.noreply.github.com>
2023-02-22 20:55:16 +0530

    All Nodejs File

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,26 +1,49 @@
-const express = require("express");
-///Express IS NOTHING BUT A COMBINATION OF ALL THE MIDDLEWARES
+const express  = require('express');
+let {people} = require('./data')
+const app = express()
+// Static index.html
+app.use(express.static('./methods-public'))
+//Parse form data
+app.use(express.urlencoded({extended : false})) // it'll help me to get the data which is been entered
 
-const app = express();
-const port = 5000;
-//////// IN MIDDLEWARE EITHER YOU PASS IT INTO NEXT  MIDDLEWARE OR YOU SEND YOUR OWN RESPONSE
-/////////// REQ => MIDDLEWARE => RESPONSE
-const logger = function (req, res, next) {
-  const method = req.method;
-  const url = req.url;
-  console.log("logging", method, url);
-  next();
-};
-// if we'll provide path in it it'll work on everything whichever has a prefix of that
-app.use(  '/api', logger) // this is used so instead of always typing logger we can still use it
-
-app.get("/", (req, res) => {
-  // Instead of writing everything and you are supposed to do in another call backs then it's best to use MIDDLEWARE
-  res.send("Hello World!");
-});
-
-app.get("/about", logger, (req, res) => {
-  res.send("I am about");
-});
-//app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.json())
+app.post('/login' , (req , res) => {
+    const {name} = req.body;
+    //console.log(req.body)
+    if(name) {
+        res.status(200).send(`Welcome to page : ${name}`)
+    }
+    res.status(401).send('User does not exist')
+})
+// app.get('/api/people' , (req , res) => {
+//     res.status(200).json({success :  true , data : people})
+// })
+// posting the name of the people
+// app.post('/api/people' , (req ,res) => {
+//     console.log(req.body)
+//     const {name} = req.body;
+//     if(!name) {
+//         res.status(201).json({success : false , msg : "Please provide valid name"}) // because in html file we used msg
+//     }
+//     res.status(201).json({success : true , person : name}) // this has to be "person" because in html we had use name as person
+// })
+app.put('/api/people/:id' , (req , res) => {
+    const {id} = req.params;
+    const {name} = req.body
+  //  console.log(id , name);
+    const person = people.find((person) => person.id === Number(id));
+    if(!person) {
+        return res.status(401).json({success : false , msg : `No person with this id `})
+    }
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)) {
+            person.name = name;
+        }
+        return people;
+    })
+    res.status(200).json({success : true , data : newPeople})
+   // res.send("Hellpw")
+})
+app.listen(5000 , () => {
+    console.log('Server is running on port 5000 .... ')
+})
