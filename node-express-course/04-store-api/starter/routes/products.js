165062ceab07a187bb55f89488b46c3b4e25a271
vinay chaurasiya <vinaychaurasiya73@gmail.com>
2023-07-02 12:23:51 +0530

    completed 04

--- node-express-course/04-store-api/starter/routes/products.js
+++ node-express-course/04-store-api/starter/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express")
 // const { route } = require("../../final/routes/products")
 const router = express.Router()
 // const {getAllProducts} = require("./controllers/products");
-const {getAllProducts} = require('../controllers/products')
+const {getAllProducts , getAllProductsStatic} = require('../controllers/products')
 router.route('/products').get(getAllProducts)
-
+router.route('/').get(getAllProductsStatic)
 module.exports = router
