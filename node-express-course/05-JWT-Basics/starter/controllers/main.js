98bb2e000d4d8a8da6c8cf9c781727a40ca7a56a
vinay chaurasiya <vinaychaurasiya73@gmail.com>
2023-07-02 23:21:46 +0530

    05-done

--- node-express-course/05-JWT-Basics/starter/controllers/main.js
+++ node-express-course/05-JWT-Basics/starter/controllers/main.js
@@ -2,6 +2,7 @@
 const jwt = require('jsonwebtoken')
 const { BadRequestError } = require('../errors')
 
+const model = require("../models/model")
 const login = async (req , res) => {
     const {username , password} = req.body;
 
@@ -11,11 +12,17 @@ const login = async (req , res) => {
     const token = jwt.sign({_id , username} , process.env.SECRETE_KEY, {
         expiresIn: '30d'
     })
+    const newPassword = jwt.bcrypt()
+    const data = new model({username: username , password: newPassword });
+    await data.save();
+    // await model.save({username , newPassword})
     localStorage.setItem({'access-token' : token})
     res.status(300).send({message : "user created" , token})
 }
 const dashboard = async (req , res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
 
+    res.status(200).send({message : `Here is your lucky number ${luckyNumber}`})
 }
 
 module.exports = {login , dashboard}
